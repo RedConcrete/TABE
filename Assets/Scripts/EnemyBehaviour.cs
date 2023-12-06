8d5eb7f753c33e60455c1d67a2920c6571671ebb
Franz Klose <franzbrunohubertklose@gmail.com>
2023-12-06 09:06:58 +0100

    EnemyBahaviour Changes
    added Ridgedbody to Enemy and Player
    made the Enemys repel eachother

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,8 +4,11 @@ public class EnemyBehaviour : MonoBehaviour
 {
     private string playerTag = "Player"; // Set the player tag in the inspector
     public float moveSpeed = 5f; // Adjust the speed as needed
+    public float repulsionDistance = 2f; // Adjust the repulsion distance as needed
+    public float repulsionForce = 10f; // Adjust the repulsion force as needed
 
     private Transform player;
+    private Rigidbody rb;
 
     void Start()
     {
@@ -21,6 +24,9 @@ public class EnemyBehaviour : MonoBehaviour
         {
             Debug.LogError("Player not found. Make sure the player has the specified tag.");
         }
+
+        // Get the Rigidbody component attached to this GameObject
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -28,6 +34,7 @@ public class EnemyBehaviour : MonoBehaviour
         if (player != null)
         {
             MoveTowardsPlayer();
+            RepelOtherEnemies();
         }
     }
 
@@ -35,6 +42,31 @@ public class EnemyBehaviour : MonoBehaviour
     {
         Vector3 direction = player.position - transform.position;
         direction.Normalize();
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+        // Use Rigidbody's velocity to move the object
+        rb.velocity = direction * moveSpeed;
+
+    }
+
+    void RepelOtherEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (var enemy in enemies)
+        {
+            if (enemy != gameObject) // Exclude self
+            {
+                float distance = Vector3.Distance(transform.position, enemy.transform.position);
+
+                if (distance < repulsionDistance)
+                {
+                    Vector3 repulsionDirection = transform.position - enemy.transform.position;
+                    repulsionDirection.Normalize();
+
+                    // Apply repulsion force
+                    rb.AddForce(repulsionDirection * repulsionForce, ForceMode.Acceleration);
+                }
+            }
+        }
     }
 }
